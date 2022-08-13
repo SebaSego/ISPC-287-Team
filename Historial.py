63aa2edf2dd8e355cc7a05ab1d67a4e53c3a6345
Agustin <julio.gonzalez.agustin@gmail.com>
2022-08-13 14:54:36 -0300

    Valoracion Final Python, ejecicio n°5

---
+++ Historial.py
@@ -0,0 +1,6 @@
+historial = (2350, 5960,23000, 1000, 8900)
+GastosTotales = int(2350 + 5960 + 23000 + 1000 + 8900)
+
+if (GastosTotales < 30000):
+    print(GastosTotales)
+else: print ("Gastos por encima de lo presupuestado")
